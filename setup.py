781f480b97df22a875bd9c97266f3862408a4364
RaccoNet <zavolokinyegor@gmail.com>
2026-03-08 13:51:55 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lite-fibonacci-gen",
+    version="1.0.0",
+    description="Lite fibonacci sequence generator with memoization",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="raccoonette",
+    url="https://github.com/MopicMP/lite-fibonacci-gen",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
